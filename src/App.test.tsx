8c9c1e204fec5721691f111b5ec1d66662138d91
Nourhan Samir <norhan.elhararyry@vodafone.com>
2022-12-11 11:22:39 +0100

    unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,28 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import  configureStore  from './Store/store';
+import '@testing-library/jest-dom';
 import React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
   Routes
 } from "react-router-dom";
-import { Header } from "./components/Common/Header";
-import { Home } from "./components/HomePage/Home";
 describe("Book Component Test", () => {
  test("test rendering book", () => {
- const bookData = {
-  id: "nggnmAEACAAJ",
-  authors: ["William E. Shotts, Jr."],
-  shelf: "currentlyReading",
-  title: "test title",
-  imageLinks: { smallThumbnail: "test path" },
-  };
  
  render(
   <Provider store={configureStore}>      <Router>
   <React.Fragment>
 </React.Fragment>
+<div>Home</div>
     <Routes>
     </Routes>
 </Router>
@@ -30,7 +22,7 @@ describe("Book Component Test", () => {
 </Provider>
 
   );
- const renderElement = screen.getByText('Read')
+ const renderElement =  screen.getByText('Home')
  expect(renderElement).toBeInTheDocument();
  });
 });
